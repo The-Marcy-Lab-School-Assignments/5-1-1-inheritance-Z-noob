3a052c827dde9d644963bd66c3f976892f3742d0
Z-noob <Zhenniwu5@gmail.com>
2024-07-11 14:14:35 -0400

    src

--- src/from-scratch.js
+++ src/from-scratch.js
@@ -1,13 +1,50 @@
 class Quadrilateral {
-
+  constructor(side1, side2, side3, side4) {
+    this.side1 = side1;
+    this.side2 = side2;
+    this.side3 = side3;
+    this.side4 = side4;
+  };
+  getPerimeter() {
+    return this.side1 + this.side2 + this.side3 + this.side4;
+  };
 }
 
-class Rectangle {
-
+class Rectangle extends Quadrilateral {
+  constructor(sideL, sideW) {
+    super(sideL, sideW, sideL, sideW)
+    this.side1 = sideL;
+    this.side2 = sideW;
+    this.side3 = sideL;
+    this.side4 = sideW;
+  };
+  // getPerimeter() {
+  //   // super.getPerimeter()
+  //   return this.side1* 2 + this.side2 * 2 
+  // };
+  getArea() {
+    return this.side1 * this.side2;
+  };
 }
 
-class Square {
-
+class Square extends Rectangle {
+  constructor(side) {
+    super(side, side)
+    this.side1 = side;
+    this.side2 = side;
+    this.side3 = side;
+    this.side4 = side;
+  };
+  // getPerimeter() {
+  //   return this.side1 * 4;
+  // };
+  // getArea() {
+  //   return this.side1 * this.side1;
+  // };
+  getDiagonal() {
+    return (Math.sqrt(2)) * this.side1;
+    //Math.sqrt((this.side1 * this.side1)*2)
+  };
 }
 
 /* Be creative with this one! */
---
+++ src/playground.js
@@ -0,0 +1,34 @@
+class Quadrilateral {
+  constructor(side1, side2, side3, side4) {
+    this.side1 = side1;
+    this.side2 = side2;
+    this.side3 = side3;
+    this.side4 = side4;
+  };
+  getPerimeter() {
+    return this.side1 + this.side2 + this.side3 + this.side4;
+  };
+}
+
+class Rectangle extends Quadrilateral {
+  constructor(sideL, sideW) {
+    super(sideL, sideW, sideL, sideW)
+    this.side1 = sideL;
+    this.side2 = sideW;
+    this.side3 = sideL;
+    this.side4 = sideW;
+  };
+  // getPerimeter() {
+  //   // super.getPerimeter()
+  //   return this.side1* 2 + this.side2 * 2 
+  // };
+  getArea() {
+    return this.side1 * this.side2;
+  };
+}
+
+const rect = new Rectangle(10, 5);
+const quad = new Quadrilateral(6, 7, 5, 9);
+// expect(rect instanceof Quadrilateral).toBe(true);
+console.log(rect.getPerimeter())
+console.log(quad.getPerimeter());
